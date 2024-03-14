02210f646e69744d327cd90959ed9857d54c0046
oalejandroelias <oalejandroelias@gmail.com>
2024-03-14 11:07:39 -0300

    Orden y filtro por campos con paginado.

--- src/views/publicacion/PublicacionList/PublicacionList.tsx
+++ src/views/publicacion/PublicacionList/PublicacionList.tsx
@@ -2,7 +2,7 @@ import reducer from './store'
 import { injectReducer } from '@/store'
 import AdaptableCard from '@/components/shared/AdaptableCard'
 import PublicacionTable from './components/PublicacionTable'
-//import ProductTableTools from './components/ProductTableTools'
+import PublicacionTableTools from './components/PublicacionTableTools'
 
 injectReducer('tablePublicacionList', reducer)
 
@@ -11,7 +11,7 @@ const PublicacionList = () => {
         <AdaptableCard className="h-full" bodyClass="h-full">
             <div className="lg:flex items-center justify-between mb-4">
                 <h3 className="mb-4 lg:mb-0">Publicaciones</h3>
-                {/* <ProductTableTools /> */}
+                <PublicacionTableTools />
             </div>
             <PublicacionTable />
         </AdaptableCard>
---
+++ src/views/publicacion/PublicacionList/components/PublicacionTableTools.tsx
@@ -0,0 +1,34 @@
+import Button from '@/components/ui/Button'
+import { HiDownload, HiPlusCircle } from 'react-icons/hi'
+import PublicacionTableSearch from './PublicacionTableSearch'
+//import Pub from './ProductFilter'
+import { Link } from 'react-router-dom'
+
+const PublicacionTableTools = () => {
+    return (
+        <div className="flex flex-col lg:flex-row lg:items-center">
+            <PublicacionTableSearch />
+            {/* <ProductFilter /> */}
+            <Link
+                download
+                className="block lg:inline-block md:mx-2 md:mb-0 mb-4"
+                to="/data/product-list.csv"
+                target="_blank"
+            >
+                <Button block size="sm" icon={<HiDownload />}>
+                    Exportar
+                </Button>
+            </Link>
+            <Link
+                className="block lg:inline-block md:mb-0 mb-4"
+                to="/app/sales/product-new"
+            >
+                <Button block variant="solid" size="sm" icon={<HiPlusCircle />}>
+                    Nuevo
+                </Button>
+            </Link>
+        </div>
+    )
+}
+
+export default PublicacionTableTools
